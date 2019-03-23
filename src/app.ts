7753378b6ff8f1fae3fe9d6773b3b741ef409797
Egor Romanov <unsaidxpl@gmail.com>
2019-03-23 09:49:02 +0300

    Connect Mongo

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
+
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PATH
+} = process.env;
 
 class App {
   public app: express.Application;
 
   constructor() {
     this.app = express();
+    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_PATH}`);
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import app from "./app";
 import apiRoutes from "./routes/api";
-
-dotenv.config();
 
 const { PORT } = process.env;
 
