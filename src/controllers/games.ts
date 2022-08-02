1144221fabd8e56ec3e9b6bd5d0de2a6623effff
Egor Romanov <unsaidxpl@gmail.com>
2022-08-02 22:57:59 +0300

    Save game to genre

--- src/controllers/games.ts
+++ src/controllers/games.ts
@@ -3,16 +3,23 @@ import { omitBy, isEmpty } from 'lodash';
 
 import Game from '../models/Game';
 
+/* Helpers */
+async function addGenresToGame(gameId: string, genreIds: Array<string>) {
+  for( const id of genreIds ){
+    await Game.updateOne({ "_id": gameId }, { $push: { genres: id }}, { new: true, useFindAndModify: false })
+  }
+}
+
 /* Actions */
 const getGames = async (req: Request, res: Response): Promise<Response> => {
-  const games = await Game.find().populate('platform');
+  const games = await Game.find().populate('platform').populate('genres');
   return res.status(200).json(games);
 };
 
 const getGame = async (req: Request, res: Response): Promise<Response> => {
   const { id } = req.params;
   try {
-    const game = await Game.findById(id);
+    const game = await Game.findById(id).populate('platform').populate('genres');
     if (!game) {
       return res.status(404).json({ message: `No game with id ${id}` });
     }
@@ -26,14 +33,17 @@ const getGame = async (req: Request, res: Response): Promise<Response> => {
 const createGame = async (req: Request, res: Response): Promise<Response> => {
   if (!req.body) return res.sendStatus(400);
 
-  const { title, year, platformId, genreIds } = req.body;
+  const { title, year, platform, genres } = req.body;
   const newGame = new Game({
     title,
     year,
-    platform: platformId
+    platform
   });
   try {
     const game = await newGame.save();
+    if (!isEmpty(genres)) {
+      await addGenresToGame(newGame._id, genres);
+    }
     return res.status(200).json(game);
   } catch (ex) {
     console.error(ex)
@@ -45,11 +55,14 @@ const updateGame = async (req: Request, res: Response): Promise<Response> => {
   if (!req.body) return res.sendStatus(400);
 
   const { id } = req.params;
-  const { title, platformId, year } = req.body;
+  const { title, platform, year, genres } = req.body;
   const game = await Game.findById(id);
-  Object.assign(game, omitBy({ title, year, platformId }, isEmpty));
+  Object.assign(game, omitBy({ title, year, platform }, isEmpty));
   try {
     const savedGame = await game.save();
+    if (!isEmpty(genres)) {
+      await addGenresToGame(savedGame._id, genres);
+    }
     return res.status(200).json(savedGame);
   } catch (ex) {
     console.error(ex)
