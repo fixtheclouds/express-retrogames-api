517fa9407f7ceb1dbbe8ad683a97513c06133497
Egor Romanov <unsaidxpl@gmail.com>
2022-08-15 09:12:33 +0300

    Move app to one file

--- src/app.ts
+++ src/app.ts
@@ -2,28 +2,35 @@ import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
 
+import apiV1Routes from '@routes/api/v1'
+
 const {
   MONGO_PORT,
   MONGO_PASSWORD,
   MONGO_PATH,
   MONGO_USER,
+  PORT
 } = process.env
 
-class App {
-  public app: express.Application
+function main(): void {
+  const app = express()
+  app.use(express.json())
 
-  constructor() {
-    this.app = express()
-    mongoose.set('debug', true)
-    mongoose.connect(
-      `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:${MONGO_PORT}/${MONGO_PATH}?authSource=admin`,
-      {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      },
-    )
-  }
+  mongoose.set('debug', true)
+  mongoose.connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:${MONGO_PORT}/${MONGO_PATH}?authSource=admin`,
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    },
+  )
+
+  app.use('/api/v1', apiV1Routes)
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.info(`Server listening on port ${PORT}`)
+  })
 }
 
-export default new App().app
+export default main()
